32efe449cff4ce1a55ae549c6d3b509355140ab8
Mexolini <maxmathijssen@gmail.com>
2024-02-28 22:44:46 +0100

    Rudimentary set up LineGraph

--- src/components/TempGraphCard/TempGraphCard.tsx
+++ src/components/TempGraphCard/TempGraphCard.tsx
@@ -1,4 +1,5 @@
-import { PropsWithChildren } from "react";
+import { useState, useEffect, useRef, PropsWithChildren } from "react";
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from "recharts";
 import { DailyForecast, HourlyForecast } from "@/typings/types";
 import styles from "./TempGraphCard.module.scss";
 import classNames from "classnames";
@@ -9,13 +10,37 @@ interface TempGraphCardProps extends PropsWithChildren {
 }
 
 function TempGraphCard({ dailyForecast, isPlaceHolder }: TempGraphCardProps) {
+  const [chartWidth, setChartWidth] = useState(0);
+  const chartContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const updateWidth = () => {
+      if (chartContainerRef.current?.clientWidth) {
+        setChartWidth(chartContainerRef.current.clientWidth);
+      }
+    };
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
   return (
     dailyForecast && (
       <div className={styles.card}>
         <div className={styles.header}>
           <h1>Temperature</h1>
         </div>
-        <div className={styles.body}></div>
+        <div className={styles.body} ref={chartContainerRef}>
+          <LineChart
+            width={chartWidth - 100}
+            height={250}
+            data={dailyForecast.hourly_forecast}
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+          </LineChart>
+        </div>
       </div>
     )
   );
