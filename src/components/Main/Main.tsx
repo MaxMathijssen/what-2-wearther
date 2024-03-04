39b2d253988a3c175864c1448606cdd1159d6984
Mexolini <maxmathijssen@gmail.com>
2024-03-05 00:52:09 +0100

    Dysfunctional wardrobe

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Forecast from "../Forecast";
 import Wardrobe from "../Wardrobe";
 import styles from "./main.module.scss";
@@ -8,12 +8,38 @@ import { WardrobeContext } from "@/providers/WardrobeProvider";
 
 function Main() {
   const { wardrobeEnabled } = useContext(WardrobeContext);
+  const [transitionState, setTransitionState] = useState("");
+
+  useEffect(() => {
+    // When wardrobeEnabled changes, set the transition state appropriately
+    if (wardrobeEnabled) {
+      setTransitionState("wardrobeEntering");
+    } else {
+      setTransitionState("forecastEntering");
+    }
+  }, [wardrobeEnabled]);
 
   return (
-    <>
-      {!wardrobeEnabled && <Forecast />}
-      {wardrobeEnabled && <Wardrobe />}
-    </>
+    <div className={styles.container}>
+      <div
+        className={`${styles.component} ${
+          transitionState === "forecastEntering"
+            ? styles.fadeInSlideInFromLeft
+            : styles.fadeOutSlideOutToLeft
+        }`}
+      >
+        <Forecast />
+      </div>
+      <div
+        className={`${styles.component} ${
+          transitionState === "wardrobeEntering"
+            ? styles.fadeInSlideInFromRight
+            : styles.fadeOutSlideOutToRight
+        }`}
+      >
+        <Wardrobe />
+      </div>
+    </div>
   );
 }
 
