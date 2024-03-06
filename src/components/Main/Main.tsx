bf73bb84a6689960863b8c27e88881d5575aec4b
Mexolini <maxmathijssen@gmail.com>
2024-03-06 23:58:50 +0100

    Working transitions

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -8,41 +8,29 @@ import { WardrobeContext } from "@/providers/WardrobeProvider";
 
 function Main() {
   const { wardrobeEnabled } = useContext(WardrobeContext);
-  const [transitionState, setTransitionState] = useState("");
+  const [transitionState, setTransitionState] = useState("initial");
   const containerRef = useRef<HTMLDivElement>(null);
   const forecastRef = useRef<HTMLDivElement>(null);
   const wardrobeRef = useRef<HTMLDivElement>(null);
-  const [containerHeight, setContainerHeight] = useState("auto");
 
   useEffect(() => {
     if (wardrobeEnabled) {
       setTransitionState("wardrobeEntering");
-    } else {
+    } else if (transitionState !== "initial") {
+      // Prevent transitions on initial load
       setTransitionState("forecastEntering");
     }
   }, [wardrobeEnabled]);
 
-  useEffect(() => {
-    const updateContainerHeight = () => {
-      let activeHeight = 0;
-      if (forecastRef.current && wardrobeRef.current) {
-        const forecastHeight = forecastRef.current.offsetHeight;
-        const wardrobeHeight = wardrobeRef.current.offsetHeight;
-        activeHeight = wardrobeEnabled ? wardrobeHeight : forecastHeight;
-      }
-      setContainerHeight(`${activeHeight}px`);
-    };
-
-    updateContainerHeight();
-  }, [wardrobeEnabled]);
-
   return (
-    <div style={{ height: containerHeight }} className={styles.outerContainer}>
+    <div style={{ height: 1495 }} className={styles.outerContainer}>
       <div ref={containerRef} className={styles.container}>
         <div
           ref={forecastRef}
           className={`${styles.component} ${
-            transitionState === "forecastEntering"
+            transitionState === "initial"
+              ? ""
+              : transitionState === "forecastEntering"
               ? styles.fadeInSlideInFromLeft
               : styles.fadeOutSlideOutToLeft
           }`}
@@ -54,6 +42,8 @@ function Main() {
           className={`${styles.component} ${
             transitionState === "wardrobeEntering"
               ? styles.fadeInSlideInFromRight
+              : transitionState === "initial"
+              ? styles.hidden
               : styles.fadeOutSlideOutToRight
           }`}
         >
