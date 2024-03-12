6b506de83b9302ff5c03e160076261ff2fae9472
Mexolini <maxmathijssen@gmail.com>
2024-03-13 00:40:49 +0100

    Added avatar placeholder

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,7 +1,42 @@
+import Image from "next/legacy/image";
 import styles from "./avatar.module.scss";
 
 function Avatar() {
-  return <div className={styles.avatarContainer}></div>;
+  return (
+    <div className={styles.avatarContainer}>
+      <div className={styles.cardContainer}>
+        <div className={styles.body}>
+          <div className={styles.topRow}>
+            <Image
+              src="/head_placeholder_bw.png"
+              width={62}
+              height={62}
+              alt="Head bald"
+              layout="fixed"
+            />
+          </div>
+          <div className={styles.middleRow}>
+            <Image
+              src="/body_placeholder_bw.png"
+              width={68}
+              height={85}
+              alt="Body t-shirt"
+              layout="fixed"
+            />
+          </div>
+          <div className={styles.bottomRow}>
+            <Image
+              src="/legs_placeholder_bw.png"
+              width={50}
+              height={100}
+              alt="Legs suit"
+              layout="fixed"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Avatar;
--- src/components/Dresser/Dresser.tsx
+++ src/components/Dresser/Dresser.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
