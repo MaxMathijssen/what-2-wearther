5c0af35be57c604fcf0d05fec62068a3a5296b5c
Mexolini <maxmathijssen@gmail.com>
2024-03-12 23:19:58 +0100

    Added WardrobeContainer card

--- src/components/WardrobeContainer/WardrobeContainer.tsx
+++ src/components/WardrobeContainer/WardrobeContainer.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import styles from "./wardrobeContainer.module.scss";
 
 function WardrobeContainer() {
-  return <div></div>;
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.card}>
+        <div className={styles.header}>
+          <h1>Wardrobe</h1>
+        </div>
+        <div className={styles.body}>
+          <div className={styles.topRow}></div>
+          <div className={styles.middleRow}></div>
+          <div className={styles.bottomRow}></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WardrobeContainer;
